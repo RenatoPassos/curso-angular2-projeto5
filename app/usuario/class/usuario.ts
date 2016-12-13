9e5b0ef6e524ea7f763eff1acca2fe9b7d65dcfe
Renato Passos <renato.passos.santos@gmail.com>
2016-12-13 13:42:38 -0200

    Classe Usuario: criação da propriedade "perfil" e de um construtor que inicializa a mesma.

--- app/usuario/class/usuario.ts
+++ app/usuario/class/usuario.ts
@@ -3,8 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */ 
+import {Perfil} from "../../perfil";
+
 export class Usuario {
     _id: number;
     nome: string;
     idade: number;
+    perfil: Perfil;
+    constructor() {
+        this.perfil = {nome: ''};
+    }
 }
